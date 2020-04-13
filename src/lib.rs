93b24fa10f880b9bb6080ed16ef4a17fe7ec7bae
Erik Zscheile <zseri.devel@ytrizja.de>
2020-04-13 13:07:23 +0200

    simplify interface

--- src/lib.rs
+++ src/lib.rs
@@ -36,57 +36,61 @@ unsafe impl<T> Sync for AtomSetOnce<T> {}
 type NextRevision<T> = Arc<AtomSetOnce<RevisionNode<T>>>;
 
 #[derive(Clone)]
-struct RevisionNode<T: Clone> {
-    data: Vec<T>,
+struct RevisionNode<T> {
+    next: NextRevision<T>,
+    data: T,
+}
+
+pub struct Queue<T> {
     next: NextRevision<T>,
 }
 
-pub struct Queue<T: Clone> {
-    pub pending: Vec<T>,
-    latest: NextRevision<T>,
+pub struct PendingMap<'a, T: 'a> {
+    pub current: &'a T,
+    pub pending: &'a mut T,
 }
 
-pub struct QueueListener<T: Clone> {
-    next: NextRevision<T>,
-}
-
-impl<T: Clone> Default for Queue<T> {
-    fn default() -> Self {
+impl<T> Clone for Queue<T> {
+    fn clone(&self) -> Self {
         Queue {
-            pending: Vec::new(),
-            latest: Arc::new(AtomSetOnce::empty()),
+            next: Arc::clone(&self.next),
         }
     }
 }
 
-impl<T: Clone> Queue<T> {
+impl<T> Default for Queue<T> {
+    fn default() -> Self {
+        Queue {
+            next: Arc::new(AtomSetOnce::empty()),
+        }
+    }
+}
+
+impl<T> Queue<T> {
     #[inline(always)]
     pub fn new() -> Self {
         Default::default()
     }
+}
 
-    pub fn listen(&self) -> QueueListener<T> {
-        QueueListener {
-            next: Arc::clone(&self.latest),
-        }
-    }
-
-    /// This method publishes the current pending events and finishes the revision
-    pub fn publish(&mut self) {
-        if self.pending.is_empty() {
-            return;
-        }
-
+impl<T: Send> Queue<T> {
+    /// This method publishes the pending event and finishes the revision,
+    /// while also calling a helper function for each skipped revision,
+    /// thus, no events are lost.
+    pub fn publish_with<F>(&mut self, pending: T, mut with_f: F)
+    where
+        F: FnMut(PendingMap<'_, T>),
+    {
         // 1. prepare revision
         let mut latest = Arc::new(AtomSetOnce::empty());
         let mut revnode = Some(Box::new(RevisionNode {
-            data: std::mem::take(&mut self.pending),
+            data: pending,
             next: Arc::clone(&latest),
         }));
 
         // 2. create dangling $self.latest
         //    (this is ok because we have a [&mut self] reference)
-        std::mem::swap(&mut latest, &mut self.latest);
+        std::mem::swap(&mut latest, &mut self.next);
         // $latest points now at the previous-latest NextRevision
 
         // 3. publish revision (e.g. append to the first 'None' ptr in the 'latest' chain)
@@ -103,25 +107,29 @@ impl<T: Clone> Queue<T> {
             }
             revnode = Some(unsafe { Box::from_raw(new) });
 
-            latest = {
+            {
+                let old: &RevisionNode<T> = unsafe { &*old };
+                with_f(PendingMap {
+                    current: &old.data,
+                    pending: &mut revnode.as_mut().unwrap().data,
+                });
+
                 // This is safe since ptr cannot be changed once it is set
                 // which means that this is now a Box.
                 // use the next revision
-                Arc::clone(&(unsafe { &*old }).next)
-            };
+                latest = Arc::clone(&old.next);
+            }
         }
     }
 
-    /// This is a convenience method to push an event into the queue
-    #[inline]
-    pub fn push(&mut self, val: T) {
-        self.pending.push(val);
+    /// This method publishes the pending event and finishes the revision
+    #[inline(always)]
+    pub fn publish(&mut self, pending: T) {
+        self.publish_with(pending, |_| {});
     }
-}
 
-impl<T: Clone> QueueListener<T> {
     /// For each revision, applies a function to the list of new events.
-    pub fn with<F: FnMut(&[T])>(&mut self, mut f: F) {
+    pub fn with<F: FnMut(&T)>(&mut self, mut f: F) {
         loop {
             let ptr = self.next.0.load(Ordering::Acquire);
             if ptr.is_null() {
@@ -131,7 +139,7 @@ impl<T: Clone> QueueListener<T> {
                     // This is safe since ptr cannot be changed once it is set
                     // which means that this is now a Arc or a Box.
                     let x: &RevisionNode<T> = mem::transmute(&*ptr);
-                    f(&x.data[..]);
+                    f(&x.data);
                     self.next = Arc::clone(&x.next);
                 }
             }
@@ -146,16 +154,14 @@ mod tests {
     #[test]
     fn queue_simple() {
         let mut q = Queue::new();
-        q.push(0);
-        q.publish();
+        q.publish(vec![0]);
 
-        let mut l = q.listen();
+        let mut l = q.clone();
         let mut marker = Vec::new();
         l.with(|evs| marker.extend(evs.iter().copied()));
         assert!(marker.is_empty());
 
-        q.push(1);
-        q.publish();
+        q.publish(vec![1]);
 
         l.with(|evs| marker.extend(evs.iter().copied()));
         assert_eq!(marker, &[1]);
@@ -164,11 +170,10 @@ mod tests {
     #[test]
     fn queue_multi() {
         let mut q = Queue::new();
-        let mut l1 = q.listen();
-        let mut l2 = q.listen();
+        let mut l1 = q.clone();
+        let mut l2 = q.clone();
 
-        q.push(0);
-        q.publish();
+        q.publish(vec![0]);
 
         let mut marker = Vec::new();
         l1.with(|evs| marker.extend(evs.iter().copied()));
@@ -185,36 +190,74 @@ mod tests {
         let mut q = Queue::new();
 
         let spt = |q: &Queue<u32>| {
-            let mut lx = q.listen();
+            let mut lx = q.clone();
             thread::spawn(move || {
                 thread::sleep(Duration::from_millis(50));
-                let mut marker = Vec::new();
-                lx.with(|evs| marker.extend(evs.iter().copied()));
-                assert_eq!(marker, &[0]);
-                marker.clear();
+                let mut marker = 0;
+                lx.with(|evs| marker += evs);
+                assert_eq!(marker, 1);
+                marker = 0;
                 thread::sleep(Duration::from_millis(20));
-                lx.with(|evs| marker.extend(evs.iter().copied()));
-                assert_eq!(marker, &[1]);
+                lx.with(|evs| marker += evs);
+                assert_eq!(marker, 2);
                 thread::sleep(Duration::from_millis(40));
                 let mut marker = Vec::new();
-                lx.with(|evs| marker.push(evs.to_vec()));
-                assert_eq!(marker, &[[2], [3]]);
+                lx.with(|evs| marker.push(*evs));
+                assert_eq!(marker, &[3, 4]);
             })
         };
 
         let th1 = spt(&q);
         let th2 = spt(&q);
-        q.push(0);
-        q.publish();
+        q.publish(1);
         thread::sleep(Duration::from_millis(60));
-        q.push(1);
-        q.publish();
+        q.publish(2);
         thread::sleep(Duration::from_millis(30));
-        q.push(2);
-        q.publish();
-        q.push(3);
-        q.publish();
+        q.publish(3);
+        q.publish(4);
         th1.join().unwrap();
         th2.join().unwrap();
+    }
+
+    #[test]
+    fn queue_mp() {
+        let mut q1 = Queue::new();
+        let mut q2 = q1.clone();
+
+        let (mut c1, mut c2) = (0, 0);
+        let (mut cl1, mut cl2) = (|pm: PendingMap<u32>| c1 += *pm.current, |pm: PendingMap<u32>| c2 += *pm.current);
+        q1.publish_with(1, &mut cl1);
+        q2.publish_with(2, &mut cl2);
+        q1.publish_with(3, &mut cl1);
+        q2.publish_with(4, &mut cl2);
+        q1.with(|cur| c1 += *cur);
+        q2.with(|cur| c2 += *cur);
+        assert_eq!(c1, 6);
+        assert_eq!(c2, 4);
+    }
+
+    #[test]
+    #[cfg_attr(miri, ignore)]
+    fn queue_mtmp() {
+        use std::{thread, time::Duration};
+        let q1 = Queue::new();
+        let q2 = q1.clone();
+
+        let spt = |mut q: Queue<u32>, publiv: Vec<u32>| {
+            thread::spawn(move || {
+                let mut c = 0;
+                for i in publiv {
+                    q.publish_with(i, |pm: PendingMap<u32>| c += *pm.current);
+                    thread::sleep(Duration::from_millis(20));
+                }
+                q.with(|cur| c += *cur);
+                c
+            })
+        };
+
+        let th1 = spt(q1, vec![1, 3]);
+        let th2 = spt(q2, vec![2, 4]);
+        assert_eq!(th1.join().unwrap(), 6);
+        assert_eq!(th2.join().unwrap(), 4);
     }
 }
